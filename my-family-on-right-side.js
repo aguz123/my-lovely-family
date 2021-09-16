d7cc193bb1de36ba522a0640e97025cbc169d00f
aguz123 <aguzkunarso@gmail.com>
2021-09-16 20:03:24 +0700

    Update my-family-on-right-side.js

--- my-family-on-right-side.js
+++ my-family-on-right-side.js
@@ -8,16 +8,3 @@
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplayformat.com/d76a89c708cd4f14f60aa1af80bf0c26/invoke.js"></scr' + 'ipt>');
 </script>
-
-<!-- Histats.com  START  (aync)-->
-<script type="text/javascript">var _Hasync= _Hasync|| [];
-_Hasync.push(['Histats.start', '1,4404850,4,0,0,0,00010000']);
-_Hasync.push(['Histats.fasi', '1']);
-_Hasync.push(['Histats.track_hits', '']);
-(function() {
-var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-hs.src = ('//s10.histats.com/js15_as.js');
-(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-})();</script>
-<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4404850&101" alt="" border="0"></a></noscript>
-<!-- Histats.com  END  -->
