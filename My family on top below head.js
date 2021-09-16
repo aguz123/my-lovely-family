0bf6cfc068ed402ce12e772d9a88e01bf5c1d7aa
aguz123 <aguzkunarso@gmail.com>
2021-09-16 19:00:39 +0700

    Create My family on top below head.js

---
+++ My family on top below head.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'ffff698d23124166e482ee4a71502ed2',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedformats.com/ffff698d23124166e482ee4a71502ed2/invoke.js"></scr' + 'ipt>');
+</script>
